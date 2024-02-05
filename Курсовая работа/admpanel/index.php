f4d04699bd45c4d848acd51ecbdd11ae716cbf28
PK303-12@gmail.com <PK303-12@gmail.com>
2024-02-05 13:12:25 +0300

    amiri

---
+++ Курсовая работа/admpanel/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+        content="width=device-width? initial-scale=1.0">
+        <title>Интернет-магазин kinder-shop.ru</title>
+        <link rel="icon" href="images/logo.png">
+</head>
+<body>
+    <header>
+        <img width="100px" height="100px"
+        src="images/logo.png" alt="Логотип">
+        <h1>Интернет- магазин kinder-shop.ru</h1>
+</header>
+<nav>
+    <a href="/ФамилияГруппа/admpanel/index.php">Административная панель</a>
+</nav>
+</body>
+</html>
